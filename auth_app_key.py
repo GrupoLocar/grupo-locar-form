b4a08c3806c9bfc1420e16c2d648c78cabda3bc7
Grupo Locar <controladoria.locar@outlook.com>
2025-06-26 10:36:02 -0300

    Ajustes de Variáveis do Vercel

--- auth_app_key.py
+++
@@ -1,7 +0,0 @@
-import webbrowser
-
-APP_KEY = 'al760okxxyn3fvg'
-url = f'https://www.dropbox.com/oauth2/authorize?client_id={APP_KEY}&' \
-      f'response_type=code&token_access_type=offline'
-
-webbrowser.open(url)
